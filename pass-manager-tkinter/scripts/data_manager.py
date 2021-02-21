3e9cd0abcd33ba97dba4485639d355d39423d318
SamDev98 <rakhesambhav@gmail.com>
2021-02-21 16:57:37 +0530

    Completed with data fetch to main screen

--- pass-manager-tkinter/scripts/data_manager.py
+++ pass-manager-tkinter/scripts/data_manager.py
@@ -9,3 +9,11 @@ class DataManager:
     def get_master_details(self):
         master_data = pandas.read_csv('data/master.csv')
         return master_data.username[0], master_data.password[0]
+
+    def get_saved_password_tags(self):
+        password_data = pandas.read_csv('data/passwords.csv')
+        tag_list = []
+        for tags in password_data.tag:
+            tag_list.append(str(tags).title())
+        tag_list = list(set(tag_list))
+        return tag_list
